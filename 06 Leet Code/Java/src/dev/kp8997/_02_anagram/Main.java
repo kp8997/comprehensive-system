6787d482fac599e9cc15c771adb1d1ac5c15692b
Khang Phan <invisibledark3@gmail.com>
2026-07-23 23:36:09 +0700

    initialize the source folder

---
+++ 06 Leet Code/Java/src/dev/kp8997/_02_anagram/Main.java
@@ -0,0 +1,13 @@
+package dev.kp8997._02_anagram;
+
+public class Main {
+    static void main() {
+
+    }
+}
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        return true;
+    }
+}
