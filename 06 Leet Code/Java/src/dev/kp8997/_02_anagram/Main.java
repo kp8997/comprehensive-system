32adef72ae5a5edd802fe5ff94d3403ddb3551cd
Khang Phan <invisibledark3@gmail.com>
2026-07-23 23:55:34 +0700

    add first solution of anagram

--- 06 Leet Code/Java/src/dev/kp8997/_02_anagram/Main.java
+++ 06 Leet Code/Java/src/dev/kp8997/_02_anagram/Main.java
@@ -1,13 +1,36 @@
 package dev.kp8997._02_anagram;
 
+import java.util.Arrays;
+
 public class Main {
     static void main() {
-
+        System.out.println(Solution.isAnagram3("anagram", "nagaram"));
     }
 }
 
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        return true;
+
+    public static boolean isAnagram2(String s, String t) {
+        return false;
+    }
+
+    public static boolean isAnagram3(String s, String t) {
+        char [] s1 = s.toCharArray();
+        char [] s2 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+
+        if (s1.length == s2.length) {
+            for (var i = 0; i < s1.length; i++) {
+                if (s1[i] != s2[i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
     }
 }
