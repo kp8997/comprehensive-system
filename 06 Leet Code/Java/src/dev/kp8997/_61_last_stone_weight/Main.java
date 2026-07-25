42e253d09117a55c4dedf493df9dc6c6c2f5be5b
Khang Phan <invisibledark3@gmail.com>
2026-07-26 01:29:57 +0700

    add solution for heap structure - easy 2 lesson

--- 06 Leet Code/Java/src/dev/kp8997/_61_last_stone_weight/Main.java
+++ 06 Leet Code/Java/src/dev/kp8997/_61_last_stone_weight/Main.java
@@ -1,13 +1,34 @@
 package dev.kp8997._61_last_stone_weight;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class Main {
     static void main() {
-
+        int[] stones = {2,7,4,1,8,1};
+        System.out.println(Solution.lastStoneWeight(stones));
     }
 }
 
 class Solution {
     public static int lastStoneWeight(int[] stones) {
-        return 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int n : stones) {
+            pq.offer(n);
+        }
+
+        while (pq.size() > 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+
+            int valueLeft = first - second;
+            if (valueLeft > 0) {
+                pq.offer(valueLeft);
+            }
+        }
+
+        return pq.isEmpty() ? 0: pq.peek();
     }
 }
