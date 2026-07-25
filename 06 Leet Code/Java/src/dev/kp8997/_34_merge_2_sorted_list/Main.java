0b5d950b2e4565b1e7b4cd944716ae5b69b0f289
Khang Phan <invisibledark3@gmail.com>
2026-07-26 04:31:32 +0700

    add merge 2 list

--- 06 Leet Code/Java/src/dev/kp8997/_34_merge_2_sorted_list/Main.java
+++ 06 Leet Code/Java/src/dev/kp8997/_34_merge_2_sorted_list/Main.java
@@ -1,4 +1,65 @@
 package dev.kp8997._34_merge_2_sorted_list;
 
+
 public class Main {
+    static void main() {
+
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = null;
+        ListNode prev = null;
+
+        while (list1 != null && list2 != null) {
+            ListNode cur;
+            if (list1.val <= list2.val) {
+                cur = list1;
+                list1 = list1.next;
+                ;
+            } else {
+                cur = list2;
+                list2 = list2.next;
+            }
+
+            if (prev == null) {
+                prev = cur;
+                head = cur;
+            } else {
+                prev.next = cur;
+                prev = prev.next;
+            }
+        }
+
+        if (list1 != null) {
+            if (prev != null) prev.next = list1;
+            else head = list1; // Handling edge case where one list was empty from start
+        } else if (list2 != null) {
+            if (prev != null) prev.next = list2;
+            else head = list2;
+        }
+        return head;
+    }
 }
