6dd63bc12c6457f7a454d0aa3ddea6feb8fab2e3
Khang Phan <invisibledark3@gmail.com>
2026-07-26 01:08:15 +0700

    add heap structure of leet code

---
+++ 06 Leet Code/Java/src/dev/kp8997/_60_k_largest_in_a_stream/Main.java
@@ -0,0 +1,68 @@
+package dev.kp8997._60_k_largest_in_a_stream;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    static void main() {
+        KthLargest kthLargest = new KthLargest(3, new int[]{4, 5, 8, 2});
+        System.out.println(kthLargest.add(3)); // return 4
+        System.out.println(kthLargest.add(5)); // return 5
+        System.out.println(kthLargest.add(10)); // return 5
+        System.out.println(kthLargest.add(9)); // return 8
+        System.out.println(kthLargest.add(4)); // return 8
+
+    }
+}
+
+class KthLargest {
+    private int k = 0;
+    private final PriorityQueue<Integer> list;
+
+    //public KthLargest(int k, int[] nums) {
+    //    this.k = k;
+    //    this.list = new PriorityQueue<>(Collections.reverseOrder());
+    //
+    //    for (int num : nums) {
+    //        this.list.offer(num);
+    //    }
+    //}
+    //
+    //public int add(int val) {
+    //    this.list.offer(val);
+    //
+    //    PriorityQueue<Integer> tempQueue = new PriorityQueue<>(this.list);
+    //
+    //    for (int i = 0; i < k - 1; i++) {
+    //        tempQueue.poll();
+    //    }
+    //
+    //    //System.out.println(tempQueue.peek());
+    //    return tempQueue.peek();
+    //}
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.list = new PriorityQueue<>(k);
+
+        for (int num : nums) {
+            this.add(num);
+        }
+    }
+
+    public int add(int val) {
+        this.list.offer(val);
+
+        if (list.size() > k) {
+            list.poll();
+        }
+
+        return list.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
---
+++ 06 Leet Code/Java/src/dev/kp8997/_61_last_stone_weight/Main.java
@@ -0,0 +1,13 @@
+package dev.kp8997._61_last_stone_weight;
+
+public class Main {
+    static void main() {
+
+    }
+}
+
+class Solution {
+    public static int lastStoneWeight(int[] stones) {
+        return 0;
+    }
+}
