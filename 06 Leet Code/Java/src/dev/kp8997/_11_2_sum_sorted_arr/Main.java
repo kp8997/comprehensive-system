136241d6e4f4a534dcbdda636cca1c97ccf1e7ab
Khang Phan <invisibledark3@gmail.com>
2026-07-25 12:41:33 +0700

    add 2 sum part 2

--- 06 Leet Code/Java/src/dev/kp8997/_11_2_sum_sorted_arr/Main.java
+++ 06 Leet Code/Java/src/dev/kp8997/_11_2_sum_sorted_arr/Main.java
@@ -1,4 +1,40 @@
 package dev.kp8997._11_2_sum_sorted_arr;
 
+import java.util.Arrays;
+
 public class Main {
+    static void main() {
+        int[] n = new int[]{2,7,11,15};
+        int[] n1 = new int[]{-1,0};
+
+        System.out.println(Arrays.toString(Solution.twoSum(n, 26)));
+        System.out.println(Arrays.toString(Solution.twoSum(n1, -1)));
+
+    }
+}
+
+class Solution {
+    public static int[] twoSum(int[] numbers, int target) {
+        // numbers is sorted in this case
+        if (numbers == null || numbers.length == 0) return new int[]{};
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            }
+            else if (sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return new int[]{};
+    }
 }
