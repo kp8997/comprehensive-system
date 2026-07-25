f04500995aad26ee3a86b18bf23851d88a016f36
Khang Phan <invisibledark3@gmail.com>
2026-07-25 20:55:25 +0700

    add 3 sum leet code

---
+++ 06 Leet Code/Java/src/dev/kp8997/_12_3_sum/Main.java
@@ -0,0 +1,103 @@
+package dev.kp8997._12_3_sum;
+
+import java.util.*;
+
+public class Main {
+    static void main() {
+        int[] nums = {-1,0,1,2,-1,-4};
+
+        System.out.println(Solution.threeSum3(nums));
+    }
+}
+
+class Solution {
+
+    public static List<List<Integer>> threeSum1(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            // Early Exit: The smallest number > 0 cannot sum to 0 in a sorted array
+            if (nums[i] > 0) {
+                break;
+            }
+
+            // Skip duplicate values for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int total = nums[i] + nums[left] + nums[right];
+
+                if (total == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    // Skip duplicate values for the second element
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    // Skip duplicate values for the third element
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (total < 0) {
+                    left++; // Total is too small, increase the sum
+                } else {
+                    right--; // Total is too large, decrease the sum
+                }
+            }
+        }
+        return res;
+    }
+    public static List<List<Integer>> threeSum2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums); // Sorting simplifies duplicate skipping
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate values for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            Set<Integer> seen = new HashSet<>();
+            for (int j = i + 1; j < nums.length; j++) {
+                int complement = -nums[i] - nums[j];
+
+                if (seen.contains(complement)) {
+                    res.add(Arrays.asList(nums[i], complement, nums[j]));
+
+                    // Skip duplicates for the second element
+                    while (j + 1 < nums.length && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                }
+                seen.add(nums[j]);
+            }
+        }
+        return res;
+    }
+
+    public static List<List<Integer>> threeSum3(int[] nums) {
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k]);
+                        Collections.sort(list);
+                        set.add(list);
+                    }
+                }
+            }
+        }
+
+        return set.stream().toList();
+    }
+}
---
+++ 06 Leet Code/Java/src/dev/kp8997/_75_letter_combinations_of_a_phone/Main.java
@@ -0,0 +1,16 @@
+package dev.kp8997._75_letter_combinations_of_a_phone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static void main() {
+
+    }
+}
+
+class Solution {
+    public static List<String> letterCombinations(String digits) {
+        return new ArrayList<>();
+    }
+}
