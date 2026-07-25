cd964e581bbce74e654659adaf86a55d81121c28
Khang Phan <invisibledark3@gmail.com>
2026-07-25 22:02:27 +0700

    add slide window

---
+++ 06 Leet Code/Java/src/dev/kp8997/_13_container_with_most_water/Main.java
@@ -0,0 +1,53 @@
+package dev.kp8997._13_container_with_most_water;
+
+public class Main {
+    static void main() {
+
+    }
+}
+
+class Solution {
+    public static int maxAreaBruteForce(int[] height) {
+        int maxWater = 0;
+        int n = height.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int width = j - i;
+                int currentHeight = Math.min(height[i], height[j]);
+                int currentWater = width * currentHeight;
+
+                maxWater = Math.max(maxWater, currentWater);
+            }
+        }
+
+        return maxWater;
+    }
+
+    public static int maxArea(int[] height) {
+        int maxWater = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            // Calculate current width and height
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+            int currentWater = width * currentHeight;
+
+            // Track maximum area
+            maxWater = Math.max(maxWater, currentWater);
+
+            // Move the pointer with the smaller height
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+}
+
+
---
+++ 06 Leet Code/Java/src/dev/kp8997/_15_best_time_to_buy_and_sell_stock/Main.java
@@ -0,0 +1,63 @@
+package dev.kp8997._15_best_time_to_buy_and_sell_stock;
+
+public class Main {
+    static void main() {
+
+    }
+}
+
+class Solution {
+
+    //Greedy (One Pass)
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price; // Update lowest buying point
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice; // Update highest profit seen so far
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static int maxProfit1(int[] prices) {
+        int bestProfit = 0;
+        int left = 0;
+        int right = 1;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                int currentProfit = prices[right] - prices[left];
+                if (currentProfit > bestProfit) {
+                    bestProfit = currentProfit;
+                }
+            } else {
+                left = right;
+            }
+
+            right++;
+        }
+
+        return bestProfit;
+    }
+
+    public static int maxProfit2(int[] prices) {
+        int len = prices.length;
+        int bestProfit = 0;
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                int tempProfit = prices[j] - prices[i];
+                if (prices[j] > prices[i] && tempProfit > bestProfit) {
+                    bestProfit = tempProfit;
+                }
+            }
+        }
+
+        return bestProfit;
+    }
+}
