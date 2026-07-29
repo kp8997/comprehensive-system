255107b6e46e82f26f767c868aed9ec5f7cb24e6
Khang Phan <invisibledark3@gmail.com>
2026-07-29 23:54:14 +0700

    add count depth of binary tree

---
+++ 06 Leet Code/Java/src/dev/kp8997/_36_maximum_depth_of_binary_tree/Main.java
@@ -0,0 +1,51 @@
+package dev.kp8997._36_maximum_depth_of_binary_tree;
+
+public class Main {
+    static void main() {
+        // Input: root = [3,9,20,null,null,15,7]
+        // Output: 3
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class Solution {
+    public static int maxDepth(TreeNode root) {
+        int depth = 0;
+        if (root != null) {
+            int leftDepth = maxDepth(root.left);
+            int rightDepth = maxDepth(root.right);
+            depth = Math.max(leftDepth, rightDepth);
+            depth++;
+        }
+        return depth;
+    }
+}
