061ca350a8b7767b9f49e40384702dbc7cc0fca1
Khang Phan <invisibledark3@gmail.com>
2026-07-31 12:49:00 +0700

    add exercise

---
+++ 05 Frontend/React/practice/_01_reduce.js
@@ -0,0 +1,9 @@
+/**
+ * @template T, U
+ * @param {(previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U} callbackFn
+ * @param {U} [initialValue]
+ * @return {U}
+ */
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+  throw 'Not implemented!';
+};
