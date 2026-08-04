992dac76b33b2792ca30585e570a1a2c4ddf06a0
Khang Phan <invisibledark3@gmail.com>
2026-08-05 04:34:10 +0700

    add solution for debounce

--- 05 Frontend/React/practice/_00_debounce.js
+++ 05 Frontend/React/practice/_00_debounce.js
@@ -4,14 +4,32 @@
  * @returns {(...args: Array<unknown>) => void}
  */
 export default function debounce(func, wait) {
-  throw 'Not implemented!';
+  let timer = null;
+  return function (...args) {
+    clearTimeout(timer);
+    const context = this;
+    timer = setTimeout(() => {
+      timer = null // reset
+      func.apply(context, args);
+    }, wait)
+  };
+  // throw "Not implemented!";
 }
 
 let i = 0;
-function increment() {
-  i++;
+function increment(step = 1) {
+  i += step;
 }
 const debouncedIncrement = debounce(increment, 100);
 
 // t = 0: Call debouncedIncrement().
 debouncedIncrement(); // i = 0
+debouncedIncrement(4);
+debouncedIncrement(4);
+
+debouncedIncrement(5);
+debouncedIncrement(5);
+
+console.log(i) // 9
+
+//
