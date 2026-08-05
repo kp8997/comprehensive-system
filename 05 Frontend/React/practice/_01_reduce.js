fe6c434471a8a11e7bfba8b1618bf8eca82cdaf8
Khang Phan <invisibledark3@gmail.com>
2026-08-06 06:35:53 +0700

    add a reduce function

--- 05 Frontend/React/practice/_01_reduce.js
+++ 05 Frontend/React/practice/_01_reduce.js
@@ -5,5 +5,55 @@
  * @return {U}
  */
 Array.prototype.myReduce = function (callbackFn, initialValue) {
-  throw 'Not implemented!';
+  // const type = typeof initialValue;
+  // console.log("initialValue ", initialValue);
+
+  if (typeof initialValue === "function") {
+    throw TypeError("initialValue error");
+  }
+  const arr = this.flat();
+
+  if (arr === null || arr === undefined) {
+    console.log("initialValue ", initialValue);
+    throw TypeError("type must be array");
+  }
+
+  if (arr.length === 0 && 
+  (initialValue === null || initialValue === undefined)) {
+    throw TypeError("must provide array or initialValue");
+  }
+  
+  if (
+    arr.length === 1 &&
+    (initialValue === null || initialValue === undefined)
+  ) {
+    return arr[0];
+  }
+
+  let accumulator;
+  let j = 0;
+
+  // if (initialValue === null || initialValue === undefined) {
+  //   if (arguments.length >= 2) {
+  //     accumulator = initialValue;
+  //   } else {
+  //     accumulator = arr[0];
+  //     j = 1;
+  //   }
+  // }  else {
+  //   accumulator = initialValue;
+  // } 
+
+  if (arguments.length >= 2) {
+      accumulator = initialValue;
+  }
+  else {
+    accumulator = arr[0];
+    j = 1;
+  }
+
+  for (let i = j; i < arr.length; i++) {
+    accumulator = callbackFn(accumulator, arr[i], i, arr);
+  }
+  return accumulator;
 };
