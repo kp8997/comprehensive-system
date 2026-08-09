807666d9170bde700b85b12348666db143bf6655
Khang Phan <invisibledark3@gmail.com>
2026-08-09 14:38:11 +0700

    rename the files for the same mapping as source exercises

---
+++ 05 Frontend/React/practice/_14_type_ultilities_2.js
@@ -0,0 +1,44 @@
+/*
+JavaScript is a dynamically typed language, which means the types of variables can change at runtime. Many interview questions involve recursively traversing objects that can hold different value types, and each type may require different handling (e.g. different code is needed to iterate over an array vs. an object). Understanding JavaScript types is crucial to solving questions like Deep Clone and Deep Equal.
+
+The Type Utilities question covered utility functions for primitive values. Implement the following utility functions to determine the types of non-primitive values.
+
+isArray(value): Return true if value is an array, false otherwise.
+isFunction(value): Return true if value is a function, false otherwise.
+isObject(value): Return true if value is an object (e.g. arrays, functions, plain objects, etc., excluding null and undefined), false otherwise.
+isPlainObject(value): Return true if value is a plain object, false otherwise (for arrays, functions, etc).
+A plain object, or what is commonly known as a Plain Old JavaScript Object (POJO), is any object whose prototype is Object.prototype or an object created via Object.create(null).
+*/
+
+
+/**
+ * @param {unknown} value
+ * @returns {boolean}
+ */
+export function isArray(value) {
+  throw 'Not implemented!';
+}
+
+/**
+ * @param {unknown} value
+ * @returns {boolean}
+ */
+export function isFunction(value) {
+  throw 'Not implemented!';
+}
+
+/**
+ * @param {unknown} value
+ * @returns {boolean}
+ */
+export function isObject(value) {
+  throw 'Not implemented!';
+}
+
+/**
+ * @param {unknown} value
+ * @returns {boolean}
+ */
+export function isPlainObject(value) {
+  throw 'Not implemented!';
+}
