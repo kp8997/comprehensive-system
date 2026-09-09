e5dc894f8312bc2675ea998dd81443630f84c449
Khang Phan <invisibledark3@gmail.com>
2026-09-10 00:46:20 +0700

    add an important note about equal?

--- 08 Ruby Concept/12 Class and Equality/document_identifier.rb
+++ 08 Ruby Concept/12 Class and Equality/document_identifier.rb
@@ -11,14 +11,25 @@ class DocumentIdentifier
   # because they are 2 different object in memory despite having same value
   def ==(other)
     return true if other.equal?(self)
-    return false unless other.instance_of?(self.class)
+    # return false unless other.instance_of?(self.class) # only type of this class
+    return false unless other.kind_of?(self.class) # allow subclasses
     folder == other.folder && name == other.name
   end
 end
 
+class ContractIdentifier < DocumentIdentifier
+
+end
+
 one = DocumentIdentifier.new('secret/plane', 'gun.txt')
 two = DocumentIdentifier.new('secret/plane', 'gun.txt')
+contract = ContractIdentifier.new('secret/plane', 'gun.txt')
 
 puts "equal" if one == two
 
 puts one == two
+
+puts 'compare contract and document'
+puts one == contract
+
+# careful, contract == null will be false since we don't implement == for null
