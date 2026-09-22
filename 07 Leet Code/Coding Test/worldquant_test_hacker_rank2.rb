dab2e44f1699e154be800c6133a3fb361837f2fa
Khang Phan <invisibledark3@gmail.com>
2026-09-22 15:56:39 +0700

    add problem for test

---
+++ 07 Leet Code/Coding Test/worldquant_test_hacker_rank2.rb
@@ -0,0 +1,16 @@
+=begin
+  
+Have articles, p, and intellectual values.
+intellectual values earn by read a article twice in a day, assume that an item in array is a article per day.
+p is the maximum value for article we can read (remember that only read twice can get a value of a article)
+Find the best intellectual we can have
+
+in = [1,2,3,4]
+ar = [1,3,4,5]
+p = 8
+
+1*2 + 3*2 = 8
+so Intellectual value we can get is 1 + 2
+
+
+=end
